8758793d4eb189e704d2594301aead21c2993ec8
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2023-09-01 22:28:17 +0300

    advanced tasks

--- binary_trees.h
+++ binary_trees.h
@@ -6,7 +6,8 @@
 
 /**
  * struct binary_tree_s - Binary tree node
- *@n: Integer stored in the node
+ *
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
